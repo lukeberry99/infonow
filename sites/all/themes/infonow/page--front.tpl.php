94ad078313a4edbef183b20893c60596ba63a4dc
Luke Berry <lukeberry99@gmail.com>
2014-10-25 06:15:53 +0100

    Some markup cleanup/refactoring

--- sites/all/themes/infonow/page--front.tpl.php
+++ sites/all/themes/infonow/page--front.tpl.php
@@ -1,426 +1,47 @@
 <!-- Main Content -->
 <?php print render($page['cmis']); ?>
 <div id="main_body">
-  <div id="body_centre">
-    <div id="area_content">
-      <div id="map">
-        <img src="<?php echo drupal_get_path('theme', 'infonow');?>/img/home_screen.jpg" alt="" />
-        <a href=""><div class="area" style="width:150px; height:100px; position:absolute; top:5.5%; left:5.5%"></div></a>
-        <div class="label_holder" style="position:absolute; top:14.2%; left:9.0%; width:100px">
-        <div class="label" style=" padding:0 0 0 16px; background-position:left center;">
-          <a href="">Human Resource</a>
-        </div>
+    <div id="body_centre">
+        <div id="area_content">
+            <div id="map">
+                <img src="<?php echo drupal_get_path('theme', 'infonow');?>/img/home_screen.jpg" alt="" />
+                <a href=""><div class="area" style="width:150px; height:100px; position:absolute; top:5.5%; left:5.5%"></div></a>
+                <div class="label_holder" style="position:absolute; top:14.2%; left:9.0%; width:100px">
+                    <div class="label" style=" padding:0 0 0 16px; background-position:left center;">
+                        <a href="">Human Resource</a>
+                    </div>
+                </div>
+            </div>
+            <!-- Sidebar -->
+            <div id="documents_holder">
+                <div class="search_holder">
+                    <div class="title_holder">
+                        <div class="title">Search</div>
+                        <div class="toggle down"></div>
+                        <div class="clear"></div>
+                    </div>
+                </div>
+                <div class="documents">
+                    <div class="layout_switcher_holder">
+                        <div class="switch gallery">
+                                <a href="?layout=gallery"></a>
+                        </div>
+                    </div>
+                    <ul class="doc_list">
+                        <li>
+                            <div class="icon">
+                                <a href="highland_risk_assessment_13_-_industrial_wiring.doc" target="_blank"><img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-word.gif" alt="DOC (Microsoft Word Document) File" title="DOC (Microsoft Word Document) File" /></a>
+                            </div>
+                            <div class="text">
+                                <div class="title"><a href="highland_risk_assessment_13_-_industrial_wiring.doc" target="_blank">Risk assessment</a></div>
+                                <div class="sub_title">Health and Safety</div>
+                            </div>
+                            <div class="clear"></div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="clear"></div>
+            </div></div>
     </div>
-</div>
-<!-- Sidebar -->
-<div id="documents_holder">
-<div class="search_holder">
-<div class="title_holder">
-<div class="title">Search</div>
-<div class="toggle down"></div>
-<div class="clear"></div>
-</div>
-<div class="search" >
-<form id="document_search" name="document_search" action="" method="post">
-<div class="fields">
-<input type="text" class="inputText" placeholder="Asset or Title" name="title" />
-<select name="type" class="inputSelect">
-<option disabled="disabled">Document type</option>
-<option value="all">All</option>
-<option value="CE Documentation" >CE Documentation</option>
-<option value="Certificate" >Certificate</option>
-<option value="Datasheet" >Datasheet</option>
-<option value="Floor Layout" >Floor Layout</option>
-<option value="General" >General</option>
-<option value="Health and Safety" >Health and Safety</option>
-<option value="Maintenance Manual" >Maintenance Manual</option>
-<option value="Operators Manual" >Operators Manual</option>
-<option value="Parts Drawing" >Parts Drawing</option>
-<option value="Parts List" >Parts List</option>
-<option value="Photographs" >Photographs</option>
-<option value="Planned Maintenance Schedule" >Planned Maintenance Schedule</option>
-<option value="Plant Layout" >Plant Layout</option>
-<option value="Product Specification" >Product Specification</option>
-<option value="Standard Operating Procedures" >Standard Operating Procedures</option>
-<option value="Technical Operators Tasks" >Technical Operators Tasks</option>
-</select>
-</div>
-<input type="submit" name="submitSearch" value="Go" class="submit" />
-<div class="clear"></div>
-</form>
-</div>
-</div>
-<div class="documents">
-<div class="layout_switcher_holder">
-<div class="switch gallery">
-<a href="?layout=gallery"><div></div></a>
-</div>
-</div>
-<ul class="doc_list">
-<li>
-<div class="icon">
-<a href="highland_risk_assessment_13_-_industrial_wiring.doc" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-word.gif" alt="DOC (Microsoft Word Document) File" title="DOC (Microsoft Word Document) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="highland_risk_assessment_13_-_industrial_wiring.doc" target="_blank">Risk assessment</a></div>
-<div class="sub_title">Health and Safety</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="risk_assess_2.doc" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-word.gif" alt="DOC (Microsoft Word Document) File" title="DOC (Microsoft Word Document) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="risk_assess_2.doc" target="_blank">Risk assessment</a></div>
-<div class="sub_title">Health and Safety</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="non_conformance_report_29.2.12.doc" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-word.gif" alt="DOC (Microsoft Word Document) File" title="DOC (Microsoft Word Document) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="non_conformance_report_29.2.12.doc" target="_blank">Example non conformance</a></div>
-<div class="sub_title">Health and Safety</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="page_under_construction.docx" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-word.gif" alt="DOC (Microsoft Word Document) File" title="DOC (Microsoft Word Document) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="page_under_construction.docx" target="_blank">Page Under construction</a></div>
-<div class="sub_title">General</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="page_under_construction.docx" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-word.gif" alt="DOC (Microsoft Word Document) File" title="DOC (Microsoft Word Document) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="page_under_construction.docx" target="_blank">Page Under construction</a></div>
-<div class="sub_title">General</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="puwer_assess.docx" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-word.gif" alt="DOC (Microsoft Word Document) File" title="DOC (Microsoft Word Document) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="puwer_assess.docx" target="_blank">PUWER assessment</a></div>
-<div class="sub_title">Health and Safety</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="page_under_construction.docx" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-word.gif" alt="DOC (Microsoft Word Document) File" title="DOC (Microsoft Word Document) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="page_under_construction.docx" target="_blank">Page Under construction</a></div>
-<div class="sub_title">General</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="page_under_construction.docx" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-word.gif" alt="DOC (Microsoft Word Document) File" title="DOC (Microsoft Word Document) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="page_under_construction.docx" target="_blank">page under construction</a></div>
-<div class="sub_title">General</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="page_under_construction.docx" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-word.gif" alt="DOC (Microsoft Word Document) File" title="DOC (Microsoft Word Document) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="page_under_construction.docx" target="_blank">Page Under construction</a></div>
-<div class="sub_title">General</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="page_under_construction.docx" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-word.gif" alt="DOC (Microsoft Word Document) File" title="DOC (Microsoft Word Document) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="page_under_construction.docx" target="_blank">Page Under construction</a></div>
-<div class="sub_title">General</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="page_under_construction.docx" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-word.gif" alt="DOC (Microsoft Word Document) File" title="DOC (Microsoft Word Document) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="page_under_construction.docx" target="_blank">Page Under construction</a></div>
-<div class="sub_title">General</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="serial_numbers_record.xlsx" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-excel.gif" alt="XLS (Microsoft Excel) File" title="XLS (Microsoft Excel) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="serial_numbers_record.xlsx" target="_blank">Serial numbers</a></div>
-<div class="sub_title">General</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="10000293e-electrical_drawing.pdf" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-pdf.gif" alt="PDF (Adobe Portable Document Format) File" title="PDF (Adobe Portable Document Format) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="10000293e-electrical_drawing.pdf" target="_blank">Electrical drawing</a></div>
-<div class="sub_title">General</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="337300_band_cooling_unit-uk-indeks--1.pdf" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-pdf.gif" alt="PDF (Adobe Portable Document Format) File" title="PDF (Adobe Portable Document Format) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="337300_band_cooling_unit-uk-indeks--1.pdf" target="_blank">Band cooling</a></div>
-<div class="sub_title">Standard Operating Procedures</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="14038.pdf" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-pdf.gif" alt="PDF (Adobe Portable Document Format) File" title="PDF (Adobe Portable Document Format) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="14038.pdf" target="_blank">Wrapper manual</a></div>
-<div class="sub_title">Maintenance Manual</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="10000293e-electrical_drawing.pdf" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-pdf.gif" alt="PDF (Adobe Portable Document Format) File" title="PDF (Adobe Portable Document Format) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="10000293e-electrical_drawing.pdf" target="_blank">Electrical drawing</a></div>
-<div class="sub_title">Parts Drawing</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="05-0_332820_automatic_band_correction_unit_-_uk.pdf" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-pdf.gif" alt="PDF (Adobe Portable Document Format) File" title="PDF (Adobe Portable Document Format) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="05-0_332820_automatic_band_correction_unit_-_uk.pdf" target="_blank">band correction unit</a></div>
-<div class="sub_title">Plant Layout</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="page_under_construction.docx" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-word.gif" alt="DOC (Microsoft Word Document) File" title="DOC (Microsoft Word Document) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="page_under_construction.docx" target="_blank">Page Under construction</a></div>
-<div class="sub_title">General</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="14038.pdf" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-pdf.gif" alt="PDF (Adobe Portable Document Format) File" title="PDF (Adobe Portable Document Format) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="14038.pdf" target="_blank">Wrapper picture</a></div>
-<div class="sub_title">Floor Layout</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="sop_43.__line_4_-__cooling_conveyor_start.stop.xls" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-excel.gif" alt="XLS (Microsoft Excel) File" title="XLS (Microsoft Excel) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="sop_43.__line_4_-__cooling_conveyor_start.stop.xls" target="_blank">Cooling conveyors</a></div>
-<div class="sub_title">Standard Operating Procedures</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="page_under_construction.docx" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-word.gif" alt="DOC (Microsoft Word Document) File" title="DOC (Microsoft Word Document) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="page_under_construction.docx" target="_blank">Page Under construction</a></div>
-<div class="sub_title">General</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="page_under_construction.docx" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-word.gif" alt="DOC (Microsoft Word Document) File" title="DOC (Microsoft Word Document) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="page_under_construction.docx" target="_blank">Page Under construction</a></div>
-<div class="sub_title">General</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="10000293e-electrical_drawing.pdf" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-pdf.gif" alt="PDF (Adobe Portable Document Format) File" title="PDF (Adobe Portable Document Format) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="10000293e-electrical_drawing.pdf" target="_blank">Electrical drawing</a></div>
-<div class="sub_title">Parts Drawing</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="control_theory_a_(3)_model_(1).pdf" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-pdf.gif" alt="PDF (Adobe Portable Document Format) File" title="PDF (Adobe Portable Document Format) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="control_theory_a_(3)_model_(1).pdf" target="_blank">Control theory</a></div>
-<div class="sub_title">Parts Drawing</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="5900157804-spare_parts.pdf" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-pdf.gif" alt="PDF (Adobe Portable Document Format) File" title="PDF (Adobe Portable Document Format) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="5900157804-spare_parts.pdf" target="_blank">Parts list</a></div>
-<div class="sub_title">Parts List</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="highland_risk_assessment_13_-_industrial_wiring.doc" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-word.gif" alt="DOC (Microsoft Word Document) File" title="DOC (Microsoft Word Document) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="highland_risk_assessment_13_-_industrial_wiring.doc" target="_blank">Risk assessment</a></div>
-<div class="sub_title">Health and Safety</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="puwer_assess.docx" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-word.gif" alt="DOC (Microsoft Word Document) File" title="DOC (Microsoft Word Document) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="puwer_assess.docx" target="_blank">PUWER assessment</a></div>
-<div class="sub_title">Health and Safety</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="page_under_construction.docx" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-word.gif" alt="DOC (Microsoft Word Document) File" title="DOC (Microsoft Word Document) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="page_under_construction.docx" target="_blank">Page Under construction</a></div>
-<div class="sub_title">Health and Safety</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="10054556-sdx40-mui-im-ab-bg_(cd).pdf" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-pdf.gif" alt="PDF (Adobe Portable Document Format) File" title="PDF (Adobe Portable Document Format) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="10054556-sdx40-mui-im-ab-bg_(cd).pdf" target="_blank">Environmental data</a></div>
-<div class="sub_title">Health and Safety</div>
-</div>
-<div class="clear"></div>
-</li>
-<li>
-<div class="icon">
-<a href="page_under_construction.docx" target="_blank">
-<img src="<?php echo drupal_get_path('theme', 'infonow'); ?>/img/icon-word.gif" alt="DOC (Microsoft Word Document) File" title="DOC (Microsoft Word Document) File" />
-</a>
-</div>
-<div class="text">
-<div class="title"><a href="page_under_construction.docx" target="_blank">company about us</a></div>
-<div class="sub_title">General</div>
-</div>
-<div class="clear"></div>
-</li>
-</ul>
-</div>
-</div>
-<div class="clear"></div>
-</div></div>
-</div>
 </div>
